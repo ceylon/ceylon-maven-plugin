c8e17a9e07a5a0379ba8468fa0fb2de8d43ce671
Tako Schotanus <tako@ceylon-lang.org>
2016-06-16 14:46:45 +0200

    Fixed test `import-dependency-force`

--- src/it/projects/import-dependency-force/verify.groovy
+++ src/it/projects/import-dependency-force/verify.groovy
@@ -1,3 +1,3 @@
-def root = new File(basedir, 'target/modules/io/netty/netty-codec/4.0.31.Final')
-return new File(root, 'io.netty.netty-codec-4.0.31.Final.jar').exists() &&
-    new File(root, 'io.netty.netty-codec-4.0.31.Final.jar.sha1').exists()
+def root = new File(basedir, 'target/modules/org/jboss/modules/jboss-modules/1.4.4.Final')
+return new File(root, 'org.jboss.modules.jboss-modules-1.4.4.Final.jar').exists() &&
+    new File(root, 'org.jboss.modules.jboss-modules-1.4.4.Final.jar.sha1').exists()
